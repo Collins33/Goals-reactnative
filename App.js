935f35c5b3810296270cbfeee24c48fa333ac37c
collins njau <collinsnjau39@gmail.com>
2020-05-23 12:13:25 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{
+      padding: 60
+    }}>
+      <View>
+        <TextInput placeholder="Enter your course goal" style={{
+          borderBottomColor: 'black',
+          borderBottomWidth: 1,
+          marginBottom: 10
+        }}/>
+        <Button title="ADD"/>
+      </View>
+      <View>
+        
+      </View>
     </View>
   );
 }
